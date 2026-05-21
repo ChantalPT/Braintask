0769fd2ad83c14d77bed41aa0e9081de0c208690
ChantalPT <chantyspinto@gmail.com>
2026-05-21 19:05:22 -0400

    refactor: Eliminar imports innecesarios en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:braintask/presentation/pages/pantalla_carga.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-//import 'auth/register_page.dart';
-import 'auth/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
